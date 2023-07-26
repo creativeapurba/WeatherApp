6e4c6a6c6edc334cbc2071a6481be35321c5185a
unknown <creativeapurba@gmail.com>
2023-07-26 20:56:35 +0530

    Handle city not found

--- index.js
+++ index.js
@@ -7,40 +7,46 @@ function search() {
     if (searchCity != "") {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`
         fetch(url).then(response => {
-            const jsonRes = response.json();
-            jsonRes.then(data => {
-                const cityName = data.name
-                const temperature = Math.round(data.main.temp);
-                const weatherType = data.weather[0].main;
-                const icon = data.weather[0].icon;
-                // To make the weather card visible
-                section.style.display = "block";
+            if (response.status == 404) {
+                alert("City Not Found");
+                section.style.display = "none";
+            }
+            else {
+                const jsonRes = response.json();
+                jsonRes.then(data => {
+                    const cityName = data.name
+                    const temperature = Math.round(data.main.temp);
+                    const weatherType = data.weather[0].main;
+                    const icon = data.weather[0].icon;
+                    // To make the weather card visible
+                    section.style.display = "block";
 
-                // Setting the City name in the weather card
-                const city = document.getElementById("city");
-                city.innerText = cityName;
+                    // Setting the City name in the weather card
+                    const city = document.getElementById("city");
+                    city.innerText = cityName;
 
-                // Setting the Temperature in the weather card
-                const temp = document.getElementById("temperature");
-                temp.innerText = temperature;
+                    // Setting the Temperature in the weather card
+                    const temp = document.getElementById("temperature");
+                    temp.innerText = temperature;
 
-                // Setting the Weather Type in the weather card
-                const weather = document.getElementById("weatherType");
-                weather.innerText = weatherType;
+                    // Setting the Weather Type in the weather card
+                    const weather = document.getElementById("weatherType");
+                    weather.innerText = weatherType;
 
-                // Setting the Current time in the weather card
-                const today = new Date();
-                const time = document.getElementById("time");
-                time.innerText = today.getHours() + ":" + today.getMinutes()
+                    // Setting the Current time in the weather card
+                    const today = new Date();
+                    const time = document.getElementById("time");
+                    time.innerText = today.getHours() + ":" + today.getMinutes()
 
-                // Setting the Icon in the weather card
-                const weatherImage = document.getElementById("weather-image");
-                weatherImage.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+                    // Setting the Icon in the weather card
+                    const weatherImage = document.getElementById("weather-image");
+                    weatherImage.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
-            })
+                })
+            }
         });
     }
-    else{
+    else {
         alert("Enter a city name")
     }
 }
